399f38368901efe7a60e5ccf159345e4821e44d8
ddj03329 <ddj03329@gmail.com>
2021-02-03 14:48:25 +0900

    첫 커밋

---
+++ src/main/java/com/hyo/untactStudent/UntactStudentApplication.java
@@ -0,0 +1,13 @@
+package com.hyo.untactStudent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UntactStudentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UntactStudentApplication.class, args);
+	}
+
+}
